f79c913e4c59453b50d3086d3ab37c9af83cac8c
Bruno Sotto <bruno@sotto.com.br>
2020-04-08 22:51:17 -0300

    script atualizado

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# exclui a public local
-rm -R ./public
-
-# exclui a public lá
-ssh root@clicaaki.com 'rm /root/projects/production/express-service-missao/public/ -R'
-echo 'excluído a public'
+# exclui a pasta de destino
+rm -R ../express-service-letras/angular-controller
 
 # build
 echo 'buildando'
 ng build
 echo 'build finalizado'
 
-# copy
-mv dist public
-scp -r ./public root@clicaaki.com:/root/projects/production/express-service-missao/public
+# move
+mv dist ../express-service-letras/angular-controller
 echo 'deploy finalizado'
-
-# exclui a public local
-rm -R ./public
